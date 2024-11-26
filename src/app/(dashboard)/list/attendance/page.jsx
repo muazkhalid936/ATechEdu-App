2ff60aa823249780b8ebae824f411d6ebf60c796
Muaz Khalid <itzmajo786@gmail.com>
2024-11-26 13:42:08 +0500

    \zx

--- src/app/(dashboard)/list/attendance/page.jsx
+++
@@ -1,8 +0,0 @@
-import { studentsData } from "../../../../lib/data";
-import Attendance from "../../../../components/Attendance";
-
-const AttendancePage = () => {
-  return <Attendance  />;
-};
-
-export default AttendancePage;
